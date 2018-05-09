2965b3c407a41607d0b28a637982ecd7eec1cdf0
twicoder <renqingwei123@163.com>
2018-05-09 10:53:15 +0800

    explicit assign bean

--- src/soundsystem/CDPlayerConfig.java
+++ src/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,20 @@
 package soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+//@ComponentScan
 public class CDPlayerConfig {
+
+    @Bean(name="lonlyHeartsClubBand")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
 }
